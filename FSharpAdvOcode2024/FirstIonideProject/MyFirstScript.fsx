30f3a0d000e3ab961bb0bcc9f766f0153e961490
gregory-crlws-tt <greg.curlewis@triremetrading.com>
2024-12-11 17:57:02 +0000

    day one is cooked

---
+++ FSharpAdvOcode2024/FirstIonideProject/MyFirstScript.fsx
@@ -0,0 +1,17 @@
+let toPigLatin (word: string) =
+    let isVowel (c: char) =
+        match c with
+        | 'a' | 'e' | 'i' | 'o' | 'u'
+        | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+        |_ -> false
+    
+    if isVowel word[0] then
+        word + "yay"
+    else
+        word[1..] + string(word[0]) + "ay"
+
+
+let sentenceToPigLatin (sentence: string) = 
+    sentence.Split(' ') |> Array.map toPigLatin |> String.concat " " |> printfn "%s"   
+
+sentenceToPigLatin "Hello, World!"
---
+++ fsharpreadME.md
@@ -0,0 +1,8 @@
+https://learn.microsoft.com/en-us/dotnet/fsharp/get-started/get-started-command-line
+
+-> get starrted creating a simple multi proj solution
+
+
+https://learn.microsoft.com/en-us/dotnet/fsharp/get-started/get-started-vscode 
+
+-> This provides an easier way to accomplish the same thing. 
